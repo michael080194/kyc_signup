776c948fe78572908fd5f7b45fafd5500273f998
michael080194 <michael080194@gmail.com>
2021-10-17 06:23:44 +0800

    5-3-1 建立後台頁面 main.php

--- admin/main.php
+++ admin/main.php
@@ -1,14 +1,12 @@
 <?php
-// 如「模組目錄」= signup，則「首字大寫模組目錄」= Signup
-// 如「資料表名」= actions，則「模組物件」= Actions
 use Xmf\Request;
-use XoopsModules\首字大寫模組目錄\模組物件;
+use XoopsModules\Kyc_signup\Kyc_signup_actions;
 
 /*-----------引入檔案區--------------*/
-$GLOBALS['xoopsOption']['template_main'] = '模組目錄_admin.tpl';
+$GLOBALS['xoopsOption']['template_main'] = 'kyc_signup_admin.tpl';
 require_once __DIR__ . '/header.php';
 require_once dirname(__DIR__) . '/function.php';
-$_SESSION['模組目錄_adm'] = true;
+$_SESSION['kyc_signup_adm'] = true;
 
 /*-----------變數過濾----------*/
 $op = Request::getString('op');
@@ -17,42 +15,45 @@ $id = Request::getInt('id');
 /*-----------執行動作判斷區----------*/
 switch ($op) {
 
-    //新增表單
-    case '資料表名_create':
-        模組物件::create();
+    //新增活動表單
+    case 'kyc_signup_actions_create':
+        Kyc_signup_actions::create();
         break;
 
-    //新增資料
-    case '資料表名_store':
-        $id = 模組物件::store();
-        header("location: {$_SERVER['PHP_SELF']}?id=$id");
+    //新增活動資料
+    case 'kyc_signup_actions_store':
+        $id = Kyc_signup_actions::store();
+        // header("location: {$_SERVER['PHP_SELF']}?id=$id");
+        redirect_header($_SERVER['PHP_SELF'] . "?id=$id", 3, "成功建立活動！");
         exit;
 
     //修改用表單
-    case '資料表名_edit':
-        模組物件::create($id);
-        $op = '資料表名_create';
+    case 'kyc_signup_actions_edit':
+        Kyc_signup_actions::create($id);
+        $op = 'kyc_signup_actions_create';
         break;
 
     //更新資料
-    case '資料表名_update':
-        模組物件::update($id);
-        header("location: {$_SERVER['PHP_SELF']}?id=$id");
+    case 'kyc_signup_actions_update':
+        Kyc_signup_actions::update($id);
+        // header("location: {$_SERVER['PHP_SELF']}?id=$id");
+        redirect_header($_SERVER['PHP_SELF'] . "?id=$id", 3, "成功修改活動！");
         exit;
 
     //刪除資料
-    case '資料表名_destroy':
-        模組物件::destroy($id);
-        header("location: {$_SERVER['PHP_SELF']}");
+    case 'kyc_signup_actions_destroy':
+        Kyc_signup_actions::destroy($id);
+        // header("location: {$_SERVER['PHP_SELF']}");
+        redirect_header($_SERVER['PHP_SELF'], 3, "成功刪除活動！");
         exit;
 
     default:
         if (empty($id)) {
-            模組物件::index();
-            $op = '資料表名_index';
+            Kyc_signup_actions::index();
+            $op = 'kyc_signup_actions_index';
         } else {
-            模組物件::show($id);
-            $op = '資料表名_show';
+            Kyc_signup_actions::show($id);
+            $op = 'kyc_signup_actions_show';
         }
         break;
 }
@@ -63,5 +64,5 @@ switch ($op) {
 $xoopsTpl->assign('now_op', $op);
 $xoTheme->addStylesheet('/modules/tadtools/css/font-awesome/css/font-awesome.css');
 $xoTheme->addStylesheet(XOOPS_URL . '/modules/tadtools/css/xoops_adm4.css');
-$xoTheme->addStylesheet(XOOPS_URL . '/modules/模組目錄/css/module.css');
+$xoTheme->addStylesheet(XOOPS_URL . '/modules/kyc_signup/css/module.css');
 require_once __DIR__ . '/footer.php';
--- admin/menu.php
+++ admin/menu.php
@@ -1,4 +1,4 @@
 <?php
 $adminmenu[] = ['title' => _MI_TAD_ADMIN_HOME, 'link' => 'admin/index.php', 'icon' => 'images/admin/home.png'];
-$adminmenu[] = ['title' => _MI_XXX_ADMENU1, 'link' => 'admin/main.php', 'icon' => 'images/admin/button.png'];
+$adminmenu[] = ['title' => '活動管理', 'link' => 'admin/main.php', 'icon' => 'images/admin/button.png'];
 $adminmenu[] = ['title' => _MI_TAD_ADMIN_ABOUT, 'link' => 'admin/about.php', 'icon' => 'images/admin/about.png'];
